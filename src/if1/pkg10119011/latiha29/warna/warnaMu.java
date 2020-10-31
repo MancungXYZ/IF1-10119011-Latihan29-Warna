726974262971f8ff520ea70949eccdd9c1e28d8f
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-31 19:08:14 +0700

    IF1-10119011-Latihan29-Warna

---
+++ src/if1/pkg10119011/latiha29/warna/warnaMu.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latiha29.warna;
+
+/**
+ *
+ * @author Reihan Wiyanda
+ */
+class warnaMu {
+
+    static String toUpperCase() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
